c87433c03eabb9dd1be72c82feb8ccd697421242
james.bradfield <jbradfield@theptrgroup.com>
2019-03-07 17:02:17 -0500

    Initial commit

---
+++ src/main/java/org/jbradfield/gilionaire/GilionaireApplication.java
@@ -0,0 +1,13 @@
+package org.jbradfield.gilionaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilionaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilionaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jbradfield/gilionaire/GilionaireApplicationTests.java
@@ -0,0 +1,16 @@
+package org.jbradfield.gilionaire;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GilionaireApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
